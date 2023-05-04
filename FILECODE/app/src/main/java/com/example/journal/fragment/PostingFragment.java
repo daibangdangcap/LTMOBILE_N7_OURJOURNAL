7bba55d017e620208efd15cca9231aca4d8e9d64
peongox <bngoc.hi4103@gmail.com>
2023-05-04 22:11:54 +0700

    ourjournal_backup_01

--- FILECODE/app/src/main/java/com/example/journal/fragment/PostingFragment.java
+++ FILECODE/app/src/main/java/com/example/journal/fragment/PostingFragment.java
@@ -26,6 +26,8 @@ import android.widget.Toast;
 
 import com.example.journal.Adapter.ImagesDangBaiAdapter;
 import com.example.journal.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
@@ -34,11 +36,14 @@ public class PostingFragment extends Fragment {
     ImageView btnBack;
     RecyclerView recyclerView;
     Button addImages_upPost;
-    ArrayList<Uri> arrayListUri=new ArrayList<Uri>();
+    ArrayList<Uri> arrayListPickedImg = new ArrayList<Uri>();
     ImagesDangBaiAdapter adapter;
     private static final int Read_Permission=101;
     private static final int PICK_IMAGE=1;
-
+    private static final int PReqCode = 1;
+    private static final int REQUESCODE = 1;
+    FirebaseAuth auth;
+    FirebaseUser currentUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,32 +52,32 @@ public class PostingFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_posting, container, false);
         btnBack=view.findViewById(R.id.btnBack_UpPost);
         recyclerView=view.findViewById(R.id.rvListAnh_DangBai);
-        adapter=new ImagesDangBaiAdapter(arrayListUri);
+        adapter=new ImagesDangBaiAdapter(arrayListPickedImg);
         GridLayoutManager gridLayoutManager=new GridLayoutManager(getActivity(),3);
         recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(adapter);
 
-
-
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
 
         addImages_upPost=view.findViewById(R.id.AddImages_UpPost);
         addImages_upPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
-                {
-                    ActivityCompat.requestPermissions(getActivity(),
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},Read_Permission);
-                    return;
+                if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)){
+                        Toast.makeText(getActivity(), "Cho phép ứng dụng truy cập thư viện ảnh của bạn nhé", Toast.LENGTH_SHORT).show();
+                    }
+
+                    else{
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PReqCode);
+                    }
                 }
-                Intent i= new Intent(Intent.ACTION_GET_CONTENT);
-                i.setType("image/*");
-                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR2);
-                {
-                    i.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+
+                else{
+                    openGallery();
                 }
-                //i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i,"Select Picture"),PICK_IMAGE);
             }
         });
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +88,7 @@ public class PostingFragment extends Fragment {
         });
         return view;
     }
+    //khi người dùng chọn ảnh
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
     {
@@ -95,16 +101,23 @@ public class PostingFragment extends Fragment {
                 for(int i=0;i<countOfImages;i++)
                 {
                     Uri imageuri=data.getClipData().getItemAt(i).getUri();
-                    arrayListUri.add(imageuri);
+                    arrayListPickedImg.add(imageuri);
                 }
                 adapter.notifyDataSetChanged();
             }
             else
             {
                 Uri imageuri=data.getData();
-                arrayListUri.add(imageuri);
+                arrayListPickedImg.add(imageuri);
             }
             adapter.notifyDataSetChanged();
         }
     }
+
+    private void openGallery(){
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
+
 }
