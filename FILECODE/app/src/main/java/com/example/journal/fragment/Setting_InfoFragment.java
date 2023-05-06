e5a7b93cbd7e74e397658c57789b7da48bad8f3b
HUYPHOENIX <truongquanghuy2003@gmail.com>
2023-05-06 12:03:11 +0700

    OurJournal v2.4.2

--- FILECODE/app/src/main/java/com/example/journal/fragment/Setting_InfoFragment.java
+++ FILECODE/app/src/main/java/com/example/journal/fragment/Setting_InfoFragment.java
@@ -3,27 +3,51 @@ package com.example.journal.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.journal.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Setting_InfoFragment extends Fragment {
 
 
     ImageView btnBack_ChangeUserInform;
+    EditText Namechange,DOBchange,Phonechange;
+    RadioButton male, female;
+    String gender;
+    Button submit;
+    FirebaseDatabase mDatabse;
+    FirebaseUser mUser;
+    RadioGroup sex;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_setting__info, container, false);
         btnBack_ChangeUserInform=view.findViewById(R.id.btnBack_ChangeInfo);
+        mDatabse = FirebaseDatabase.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        Namechange = view.findViewById(R.id.edUserName_ChangeInfo);
+        DOBchange = view.findViewById(R.id.edDOB_ChangeInfo);
+        Phonechange = view.findViewById(R.id.edPhone_ChangeInfo);
+        submit = view.findViewById(R.id.btnSubmic_ChangeInfo);
+        male = view.findViewById(R.id.rbMale_ChangeInfo);
+        female = view.findViewById(R.id.rbFemale_ChangeInfo);
+        sex = view.findViewById(R.id.rgSex_ChangeInfo);
         btnBack_ChangeUserInform.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,6 +56,34 @@ public class Setting_InfoFragment extends Fragment {
                 {
                     Navigation.findNavController(view).popBackStack();
                 }
+            }
+
+        });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Namechange.getText().toString().trim();
+                String dob = DOBchange.getText().toString().trim();
+                String phone = Phonechange.getText().toString().trim();
+                String Trai = male.getText().toString().trim();
+                String Gai = female.getText().toString().trim();
+                if(male.isChecked())
+                {
+                    gender = Trai;
+                }
+                if(female.isChecked())
+                {
+                    gender = Gai;
+                }
+                // Lấy reference đến node người dùng trong database
+                DatabaseReference userRef = mDatabse.getReference("Users").child(mUser.getUid());
+                // Cập nhật thông tin người dùng
+                userRef.child("fullname").setValue(name);
+                userRef.child("dob").setValue(dob);
+                userRef.child("gender").setValue(gender);
+                userRef.child("phone").setValue(phone);
+
+                Toast.makeText(getActivity(), "Thông tin người dùng đã được cập nhật thành công", Toast.LENGTH_SHORT).show();
             }
         });
         return view;
--- FILECODE/app/src/main/java/com/example/journal/fragment/Setting_PasswordFragment.java
+++ FILECODE/app/src/main/java/com/example/journal/fragment/Setting_PasswordFragment.java
@@ -1,39 +1,100 @@
 package com.example.journal.fragment;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.journal.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 
 public class Setting_PasswordFragment extends Fragment {
 
     ImageView btnBack_ChangePassword;
-
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    EditText currentpass , newpass;
+    Button submit;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_setting__password, container, false);
-        btnBack_ChangePassword=view.findViewById(R.id.btnBack_ChangePassword);
+        View view = inflater.inflate(R.layout.fragment_setting__password, container, false);
+        btnBack_ChangePassword = view.findViewById(R.id.btnBack_ChangePassword);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        // Lấy reference đến các view trong fragment
+        currentpass = view.findViewById(R.id.edPresentPassword_ChangePassword);
+        newpass = view.findViewById(R.id.edNewwPassword_ChangePassword);
+        submit = view.findViewById(R.id.btnSubmic_ChangePassword);
         btnBack_ChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment fragment=new SettingFragment();
-                if(fragment!=null)
-                {
+                Fragment fragment = new SettingFragment();
+                if (fragment != null) {
                     Navigation.findNavController(view).popBackStack();
                 }
             }
         });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentPassword = currentpass.getText().toString().trim();
+                String newPassword = newpass.getText().toString().trim();
+
+                // Kiểm tra xem mật khẩu hiện tại có chính xác hay không
+                AuthCredential credential = EmailAuthProvider.getCredential(mUser.getEmail(), currentPassword);
+                mUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Đổi mật khẩu
+                            mUser.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getActivity(), "Mật khẩu đã được đổi thành công", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(getActivity(), "Có lỗi xảy ra khi đổi mật khẩu", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(getActivity(), "Mật khẩu hiện tại không chính xác", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
         return view;
     }
+
 }
